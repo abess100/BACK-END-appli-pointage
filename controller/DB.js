5c9f3320da5dbf4a492624fcf3c5fe3122dea5a3
abess100 <abessolodavyrobson@gmail.com>
2024-01-12 14:36:36 +0100

    modif 522

--- controller/DB.js
+++ controller/DB.js
@@ -2,10 +2,16 @@ const express = require('express')
 const mysql = require('mysql2')
 
 const connexion = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
+    // host: 'localhost',
+    // user:'root',
+    // password: '#Abess1000',
+    // database:'xpoint'
+    
+    host: 'mysql-abess.alwaysdata.net',
+    user:'abess',
     password: '#Abess1000',
-    database:'xpoint'
+    database:'abess_xpoint'
+
 })
 
 connexion.connect((error) =>{
